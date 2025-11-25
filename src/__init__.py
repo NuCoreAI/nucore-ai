d2c3002b322cd4af148440b47182139753bc2dae
Michel Kohanim <michel@universal-devices.com>
2025-11-25 15:49:17 -0800

    1.1.2

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from rag import __all__ as all_rag
 from config import __all__ as config_all
 from nucore import __all__ as nucore_all
+from iox import __all__ as iox_all
 
-__all__ = all_rag + config_all + nucore_all
+__all__ = all_rag + config_all + nucore_all + iox_all
