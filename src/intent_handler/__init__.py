f04cec18c5e2e2dcad489008c2c71213e73c86cb
Michel Kohanim <michel@universal-devices.com>
2026-05-29 14:33:46 -0700

    added agent handling

--- src/intent_handler/__init__.py
+++ src/intent_handler/__init__.py
@@ -1,7 +1,8 @@
 from .base import BaseIntentHandler
+from .bounded_agentic import BoundedAgentOrchestrator, BoundedAgentPolicy, BoundedAgentPolicyConfig
 from .dispatch_builder import build_default_dispatch_adapter
 from .loader import IntentHandlerRegistry
-from .models import ConversationHistory, ConversationTurn, IntentDefinition, IntentHandlerResult, RouteResult
+from .models import AgentBudget, AgentStepLog, ConversationHistory, ConversationTurn, IntentDefinition, IntentHandlerResult, ModeDecision, RouteResult
 from .provider_dispatch_adapter import ProviderDispatchLLMAdapter
 from .router import IntentRouter
 from .runtime import IntentRuntime, _load_runtime_config
@@ -11,7 +12,12 @@ from .directory_monitor import DirectoryChangeEvent, DirectoryMonitor
 
 __all__ = [
     "BaseIntentHandler",
+    "BoundedAgentOrchestrator",
+    "BoundedAgentPolicy",
+    "BoundedAgentPolicyConfig",
     "build_default_dispatch_adapter",
+    "AgentBudget",
+    "AgentStepLog",
     "AnthropicProviderClient",
     "ConversationHistory",
     "ConversationTurn",
@@ -23,6 +29,7 @@ __all__ = [
     "IntentDefinition",
     "IntentHandlerRegistry",
     "IntentHandlerResult",
+    "ModeDecision",
     "IntentRouter",
     "IntentRuntime",
     "_load_runtime_config",
